072c8e0506829255d5d1850f8bb5ac3b5f41745a
giovaniCheloni <130862039+giovaniCheloni@users.noreply.github.com>
2024-07-03 18:54:04 -0300

    HomePage e instalação do Expo

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
-//teste github
+import React from 'react';
+import {View, Text} from 'react-native';
+import HomePage from './src/pages/HomePage';
+
+const App = () => {
+
+    return (
+        <HomePage />
+    );
+
+}
+
+export default App;
